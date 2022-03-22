efad9e9f59679d350564d79186b472ebca1d0d77
Maria <maria.anita.stickel@gmx.de>
2022-03-22 21:38:51 +0100

    changed values for measurements

--- agent_code/ml_agent_1_m3/train.py
+++ agent_code/ml_agent_1_m3/train.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 # Hyper parameters 
 ALPHA =         0.0001
-GAMMA =         0.8
+GAMMA =         0.7
 BUFFER_SIZE =   100
 BATCH_SIZE =    50
 # epsilon is found in callbacks.py
